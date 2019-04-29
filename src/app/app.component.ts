e8e71ebd4106bab9472e83bbadbca156a6df8155
Nagarjuna Yadav K <nagarjuna.yadav.k_1@philips.com>
2019-04-29 23:08:46 +0530

    Angular Fire base Notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MessagingService } from "./shared/messaging.service";
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firebase-notification';
+  message;
+
+  constructor(private messagingService: MessagingService, public afAuth: AngularFireAuth) { }
+
+  ngOnInit() {
+    const userId = 'sZLmTLvquyVllYJA0Msw';
+    
+    this.messagingService.requestPermission(userId)
+    this.messagingService.receiveMessage()
+    this.message = this.messagingService.currentMessage
+  }
+
+  login() {
+    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
+  }
+  logout() {
+    this.afAuth.auth.signOut();
+  }
 }
